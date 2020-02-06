8995fcde696c1d0f6c7ee7c9e063d28e2c386964
Luca Montaigut <lmontaigut@gmail.com>
2020-02-06 17:41:32 +0100

    Faker database

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,4 +1,5 @@
 class Doctor < ApplicationRecord
+  belongs_to :city
   has_many :appointments
   has_many :patients, through: :appointments
   has_many :join_table_doctor_specialties
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,18 +14,43 @@ City.destroy_all
 JoinTableDoctorSpecialty.destroy_all
 Specialty.destroy_all
 
-#100.times do
-City.create(city: "Nantes")
+10.times do
+City.create(city: Faker::Address.city)
+end
 
-Specialty.create(specialty: "Anesthesit")
-Specialty.create(specialty: "Cardiologue")
+7.times do
+Specialty.create(specialty: Faker::Job.field)
+end
 
-Doctor.create(first_name: "Jean-Marie", last_name: "Hubert", zip_code: "44000", city_id: City.first.id)
+15.times do
+Doctor.create(
+  first_name: Faker::Name.first_name, 
+  last_name: Faker::Name.last_name, 
+  zip_code: Faker::Address.zip_code, 
+  city_id: City.find(rand(City.first.id..City.last.id)).id
+)
+end
 
-JoinTableDoctorSpecialty.create(doctor_id: Doctor.first.id, specialty_id: Specialty.first.id)
-JoinTableDoctorSpecialty.create(doctor_id: Doctor.first.id, specialty_id: Specialty.last.id)
+36.times do
+JoinTableDoctorSpecialty.create(
+  doctor_id: Doctor.find(rand(Doctor.first.id..Doctor.last.id)).id, 
+  specialty_id: Specialty.find(rand(Specialty.first.id..Specialty.last.id)).id
+)
+end
 
-Patient.create(first_name: "Claude", last_name: "Dubois", city_id: City.first.id)
+74.times do
+Patient.create(
+  first_name: Faker::Name.first_name, 
+  last_name: Faker::Name.last_name, 
+  city_id: City.find(rand(City.first.id..City.last.id)).id
+)
+end
 
-Appointment.create(date: Time.now, doctor_id: Doctor.first.id, patient_id: Patient.first.id, city_id: City.first.id)
-
+140.times do
+Appointment.create(
+  date: Faker::Time.between(from: DateTime.now, to: DateTime.now + 50), 
+  doctor_id: Doctor.find(rand(Doctor.first.id..Doctor.last.id)).id, 
+  patient_id: Patient.find(rand(Patient.first.id..Patient.last.id)).id,
+  city_id: City.find(rand(City.first.id..City.last.id)).id
+)
+end
